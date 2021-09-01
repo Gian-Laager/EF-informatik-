c94d63b788cbdf816453c5b0bfa880d193c391af
Gian Laager <gian.laager@source-graphics.ch>
2021-09-01 08:23:55 +0200

    solved excersise 3.5.2

--- DecToBinary/app/Main.hs
+++ DecToBinary/app/Main.hs
@@ -1,6 +1,17 @@
 module Main where
 
 import Lib
+import System.IO (putStr)
+import GHC.IO (evaluate)
+
+decToHex number = algorithm number [] where
+    algorithm 1 parsed = 1 : parsed
+    algorithm 0 parsed = 0 : parsed
+    algorithm number parsed = algorithm (number `div` 2) $ (number `mod` 2) : parsed
 
 main :: IO ()
-main = someFunc
+main = do
+    numberString <- getLine
+    let dec = read numberString :: Int
+    print $ decToHex dec
+    main
