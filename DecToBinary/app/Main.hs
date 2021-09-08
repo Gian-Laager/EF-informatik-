8cc3535e547c4159ef4e7a635335689448e8bde5
Gian Laager <gian.laager@source-graphics.ch>
2021-09-08 08:44:21 +0200

    added dec to hex

--- DecToBinary/app/Main.hs
+++ DecToBinary/app/Main.hs
@@ -3,14 +3,22 @@ module Main where
 import Lib
 import System.IO (putStr)
 
-decToHex number = algorithm number [] where
+decToBin number = algorithm number [] where
     algorithm 1 parsed = 1 : parsed
     algorithm 0 parsed = 0 : parsed
     algorithm number parsed = algorithm (number `div` 2) $ (number `mod` 2) : parsed
+    
+hexCharacters :: [Char]
+hexCharacters = ['0', '1', '2', '3', '4', '5', '6','7','8','9','A','B','C', 'D','E','F']
+    
+decToHex :: Int -> String
+decToHex number = algorithm number [] where 
+    algorithm number parsed | number < 0x10 = hexCharacters !! number : parsed
+                            | otherwise = algorithm (number `div` 0x10) ((hexCharacters !! (number `mod` 0x10)) : parsed)
 
 main :: IO ()
 main = do
-    numberString <- getLine
+    numberString <- getLine 
     let dec = read numberString :: Int
-    print $ decToHex dec
+    putStrLn $ decToHex dec
     main
