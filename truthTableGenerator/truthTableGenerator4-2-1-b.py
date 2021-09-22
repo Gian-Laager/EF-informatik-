5dd3d010369c4d57c42e8cd45eafe706a8d03a4f
Gian Laager <gian.laager@source-graphics.ch>
2021-09-22 08:57:01 +0200

    solved excersise 4.2.1 b

---
+++ truthTableGenerator/truthTableGenerator4-2-1-b.py
@@ -0,0 +1,18 @@
+values = [True, False]
+
+
+
+print("a\t|\tb\t|\tc\t|\td\t||\te\t|\tf\t|\ty")
+print("#####################################################################################################")
+for a in values:
+    for b in values:
+        for c in values:
+            for d in values:
+                e = a or b
+                f = not (c and b)
+                g = f ^ e
+                y = d ^ g
+                print(a, b, c, d, sep="\t|\t", end="")
+                print("\t||\t", end="")
+                print(e, f, y, sep="\t|\t")
+                print("-----------------------------------------------------------------------------------------------------")
